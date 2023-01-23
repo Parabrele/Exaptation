50905e464463e174cb2644a8d48fa23dfd5a2846
Parabrele <g.dhimoila@gmail.com>
2023-01-23 22:47:31 +0100

    neelgpt

---
+++ NeelGPT.py
@@ -0,0 +1,394 @@
+try:
+  import google.colab
+  IN_COLAB = True
+  print("Running as a Colab notebook")
+  %pip install git+https://github.com/neelnanda-io/Easy-Transformer.git@clean-transformer-demo
+  # Install another version of node that makes PySvelte work way faster
+  !curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -; sudo apt-get install -y nodejs  
+  %pip install git+https://github.com/neelnanda-io/PySvelte.git
+  %pip install fancy_einsum
+  %pip install einops
+except:
+  IN_COLAB = False
+  print("Running as a Jupyter notebook - intended for development only!")
+
+
+
+import einops
+from fancy_einsum import einsum
+from dataclasses import dataclass
+from easy_transformer import EasyTransformer
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+from easy_transformer.utils import get_corner, gelu_new, tokenize_and_concatenate
+import tqdm.auto as tqdm
+
+reference_gpt2 = EasyTransformer.from_pretrained("gpt2-small", fold_ln=False, center_unembed=False, center_writing_weights=False)
+
+sorted_vocab = sorted(list(reference_gpt2.tokenizer.vocab.items()), key=lambda n:n[1])
+print(sorted_vocab[:20])
+print()
+print(sorted_vocab[250:270])
+print()
+print(sorted_vocab[990:1010])
+print()
+
+
+
+sorted_vocab[-20:]
+
+
+
+print(reference_gpt2.to_tokens("Whether a word begins with a capital or space matters!"))
+print(reference_gpt2.to_tokens("Whether a word begins with a capital or space matters!", prepend_bos=False))
+
+
+
+print(reference_gpt2.to_str_tokens("Ralph"))
+print(reference_gpt2.to_str_tokens(" Ralph"))
+print(reference_gpt2.to_str_tokens(" ralph"))
+print(reference_gpt2.to_str_tokens("ralph"))
+
+
+
+reference_gpt2.to_str_tokens("56873+3184623=123456789-1000000000")
+
+
+
+reference_text = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+tokens = reference_gpt2.to_tokens(reference_text)
+print(tokens)
+print(tokens.shape)
+print(reference_gpt2.to_str_tokens(tokens))
+
+
+
+tokens = tokens.cuda()
+logits, cache = reference_gpt2.run_with_cache(tokens)
+print(logits.shape)
+
+
+
+
+log_probs = logits.log_softmax(dim=-1)
+probs = logits.log_softmax(dim=-1)
+print(log_probs.shape)
+print(probs.shape)
+
+
+
+list(zip(reference_gpt2.to_str_tokens(reference_text), reference_gpt2.tokenizer.batch_decode(logits.argmax(dim=-1)[0])))
+
+
+
+next_token = logits[0, -1].argmax(dim=-1)
+print(next_token)
+
+
+
+next_tokens = torch.cat([tokens, torch.tensor(next_token, device='cuda', dtype=torch.int64)[None, None]], dim=-1)
+new_logits = reference_gpt2(next_tokens)
+print("New Input:", next_tokens)
+print(next_tokens.shape)
+print("New Input:", reference_gpt2.tokenizer.decode(next_tokens[0]))
+
+print(new_logits.shape)
+print(new_logits[-1, -1].argmax(-1))
+
+print(reference_gpt2.tokenizer.decode(new_logits[-1, -1].argmax(-1)))
+
+
+
+for activation_name, activation in cache.cache_dict.items():
+    # Only print for first layer
+    if ".0." in activation_name or "blocks" not in activation_name:
+        print(activation_name, activation.shape)
+
+
+
+for name, param in reference_gpt2.named_parameters():
+    # Only print for first layer
+    if ".0." in name or "blocks" not in name:
+        print(name, param.shape)
+
+
+
+# As a reference - note there's a lot of stuff we don't care about in here, to do with library internals or other architectures
+print(reference_gpt2.cfg)
+
+
+
+
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+cfg = Config()
+print(cfg)
+
+
+
+def rand_float_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).cuda()
+    random_input = torch.randn(shape).cuda()
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    print("Output shape:", output.shape)
+    print()
+    return output
+
+def rand_int_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).cuda()
+    random_input = torch.randint(100, 1000, shape).cuda()
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    print("Output shape:", output.shape)
+    print()
+    return output
+
+def load_gpt2_test(cls, gpt2_layer, input_name, cache_dict=cache.cache_dict):
+    cfg = Config(debug=True)
+    layer = cls(cfg).cuda()
+    layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+    # Allow inputs of strings or tensors
+    if isinstance(input_name, str): 
+        reference_input = cache_dict[input_name]
+    else:
+        reference_input = input_name
+    print("Input shape:", reference_input.shape)
+    output = layer(reference_input)
+    print("Output shape:", output.shape)
+    reference_output = gpt2_layer(reference_input)
+    print("Reference output shape:", reference_output.shape)
+
+    comparison = torch.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum()/comparison.numel():.2%} of the values are correct")
+    return output
+
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(torch.ones(cfg.d_model))
+        self.b = nn.Parameter(torch.zeros(cfg.d_model))
+    
+    def forward(self, residual):
+        # residual: [batch, position, d_model]
+        if self.cfg.debug: print("Residual:", residual.shape)
+        residual = residual - einops.reduce(residual, "batch position d_model -> batch position 1", "mean")
+        # Calculate the variance, square root it. Add in an epsilon to prevent divide by zero.
+        scale = (einops.reduce(residual.pow(2), "batch position d_model -> batch position 1", "mean") + cfg.layer_norm_eps).sqrt()
+        normalized = residual / scale
+        normalized = normalized * self.w + self.b
+        if self.cfg.debug: print("Normalized:", residual.shape)
+        return normalized
+
+
+
+_ = rand_float_test(LayerNorm, [2, 4, 768])
+_ = load_gpt2_test(LayerNorm, reference_gpt2.ln_final, "blocks.11.hook_resid_post")
+
+
+
+class Embed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_E = nn.Parameter(torch.empty((cfg.d_vocab, cfg.d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+    
+    def forward(self, tokens):
+        # tokens: [batch, position]
+        if self.cfg.debug: print("Tokens:", tokens.shape)
+        embed = self.W_E[tokens, :] # [batch, position, d_model]
+        if self.cfg.debug: print("Embeddings:", embed.shape)
+        return embed
+
+rand_int_test(Embed, [2, 4])
+load_gpt2_test(Embed, reference_gpt2.embed, tokens)
+
+
+
+class PosEmbed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_pos = nn.Parameter(torch.empty((cfg.n_ctx, cfg.d_model)))
+        nn.init.normal_(self.W_pos, std=self.cfg.init_range)
+    
+    def forward(self, tokens):
+        # tokens: [batch, position]
+        if self.cfg.debug: print("Tokens:", tokens.shape)
+        pos_embed = self.W_pos[:tokens.size(1), :] # [position, d_model]
+        pos_embed = einops.repeat(pos_embed, "position d_model -> batch position d_model", batch=tokens.size(0))
+        if self.cfg.debug: print("pos_embed:", pos_embed.shape)
+        return pos_embed
+
+rand_int_test(PosEmbed, [2, 4])
+load_gpt2_test(PosEmbed, reference_gpt2.pos_embed, tokens)
+
+
+
+import pysvelte
+pysvelte.AttentionMulti(tokens=reference_gpt2.to_str_tokens(reference_text), attention=cache['blocks.0.attn.hook_attn'][0].permute(1, 2, 0)).show()
+
+
+
+class Attention(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_Q = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        nn.init.normal_(self.W_Q, std=self.cfg.init_range)
+        self.b_Q = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        self.W_K = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        nn.init.normal_(self.W_K, std=self.cfg.init_range)
+        self.b_K = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        self.W_V = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        nn.init.normal_(self.W_V, std=self.cfg.init_range)
+        self.b_V = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        
+        self.W_O = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_head, cfg.d_model)))
+        nn.init.normal_(self.W_O, std=self.cfg.init_range)
+        self.b_O = nn.Parameter(torch.zeros((cfg.d_model)))
+        
+        self.register_buffer("IGNORE", torch.tensor(-1e5, dtype=torch.float32, device="cuda"))
+    
+    def forward(self, normalized_resid_pre):
+        # normalized_resid_pre: [batch, position, d_model]
+        if self.cfg.debug: print("Normalized_resid_pre:", normalized_resid_pre.shape)
+        
+        q = einsum("batch query_pos d_model, n_heads d_model d_head -> batch query_pos n_heads d_head", normalized_resid_pre, self.W_Q) + self.b_Q
+        k = einsum("batch key_pos d_model, n_heads d_model d_head -> batch key_pos n_heads d_head", normalized_resid_pre, self.W_K) + self.b_K
+        
+        attn_scores = einsum("batch query_pos n_heads d_head, batch key_pos n_heads d_head -> batch n_heads query_pos key_pos", q, k)
+        attn_scores = attn_scores / math.sqrt(self.cfg.d_head)
+        attn_scores = self.apply_causal_mask(attn_scores)
+
+        pattern = attn_scores.softmax(dim=-1) # [batch, n_head, query_pos, key_pos]
+
+        v = einsum("batch key_pos d_model, n_heads d_model d_head -> batch key_pos n_heads d_head", normalized_resid_pre, self.W_V) + self.b_V
+
+        z = einsum("batch n_heads query_pos key_pos, batch key_pos n_heads d_head -> batch query_pos n_heads d_head", pattern, v)
+
+        attn_out = einsum("batch query_pos n_heads d_head, n_heads d_head d_model -> batch query_pos d_model", z, self.W_O) + self.b_O
+        return attn_out
+
+    def apply_causal_mask(self, attn_scores):
+        # attn_scores: [batch, n_heads, query_pos, key_pos]
+        mask = torch.triu(torch.ones(attn_scores.size(-2), attn_scores.size(-1), device=attn_scores.device), diagonal=1).bool()
+        attn_scores.masked_fill_(mask, self.IGNORE)
+        return attn_scores
+
+rand_float_test(Attention, [2, 4, 768])
+load_gpt2_test(Attention, reference_gpt2.blocks[0].attn, cache["blocks.0.ln1.hook_normalized"])
+
+
+
+class MLP(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_in = nn.Parameter(torch.empty((cfg.d_model, cfg.d_mlp)))
+        nn.init.normal_(self.W_in, std=self.cfg.init_range)
+        self.b_in = nn.Parameter(torch.zeros((cfg.d_mlp)))
+        self.W_out = nn.Parameter(torch.empty((cfg.d_mlp, cfg.d_model)))
+        nn.init.normal_(self.W_out, std=self.cfg.init_range)
+        self.b_out = nn.Parameter(torch.zeros((cfg.d_model)))
+    
+    def forward(self, normalized_resid_mid):
+        # normalized_resid_mid: [batch, position, d_model]
+        if self.cfg.debug: print("Normalized_resid_mid:", normalized_resid_mid.shape)
+        pre = einsum("batch position d_model, d_model d_mlp -> batch position d_mlp", normalized_resid_mid, self.W_in) + self.b_in
+        post = gelu_new(pre)
+        mlp_out = einsum("batch position d_mlp, d_mlp d_model -> batch position d_model", post, self.W_out) + self.b_out
+        return mlp_out
+
+rand_float_test(MLP, [2, 4, 768])
+load_gpt2_test(MLP, reference_gpt2.blocks[0].mlp, cache["blocks.0.ln2.hook_normalized"])
+
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+        self.ln1 = LayerNorm(cfg)
+        self.attn = Attention(cfg)
+        self.ln2 = LayerNorm(cfg)
+        self.mlp = MLP(cfg)
+    
+    def forward(self, resid_pre):
+        # resid_pre [batch, position, d_model]
+        normalized_resid_pre = self.ln1(resid_pre)
+        attn_out = self.attn(normalized_resid_pre)
+        resid_mid = resid_pre + attn_out
+        
+        normalized_resid_mid = self.ln2(resid_mid)
+        mlp_out = self.mlp(normalized_resid_mid)
+        resid_post = resid_mid + mlp_out
+        return resid_post
+rand_float_test(TransformerBlock, [2, 4, 768])
+load_gpt2_test(TransformerBlock, reference_gpt2.blocks[0], cache["resid_pre", 0])
+
+
+
+class Unembed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_U = nn.Parameter(torch.empty((cfg.d_model, cfg.d_vocab)))
+        nn.init.normal_(self.W_U, std=self.cfg.init_range)
+        self.b_U = nn.Parameter(torch.zeros((cfg.d_vocab), requires_grad=False))
+    
+    def forward(self, normalized_resid_final):
+        # normalized_resid_final [batch, position, d_model]
+        if self.cfg.debug: print("Normalized_resid_final:", normalized_resid_final.shape)
+        logits = einsum("batch position d_model, d_model d_vocab -> batch position d_vocab", normalized_resid_final, self.W_U) + self.b_U
+        return logits
+
+rand_float_test(Unembed, [2, 4, 768])
+load_gpt2_test(Unembed, reference_gpt2.unembed, cache["ln_final.hook_normalized"])
+
+
+
+class DemoTransformer(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.embed = Embed(cfg)
+        self.pos_embed = PosEmbed(cfg)
+        self.blocks = nn.ModuleList([TransformerBlock(cfg) for _ in range(cfg.n_layers)])
+        self.ln_final = LayerNorm(cfg)
+        self.unembed = Unembed(cfg)
+    
+    def forward(self, tokens):
+        # tokens [batch, position]
+        embed = self.embed(tokens)
+        pos_embed = self.pos_embed(tokens)
+        residual = embed + pos_embed
+        for block in self.blocks:
+            residual = block(residual)
+        normalized_resid_final = self.ln_final(residual)
+        logits = self.unembed(normalized_resid_final)
+        # logits have shape [batch, position, logits]
+        return logits
+
+rand_int_test(DemoTransformer, [2, 4])
+load_gpt2_test(DemoTransformer, reference_gpt2, tokens)
